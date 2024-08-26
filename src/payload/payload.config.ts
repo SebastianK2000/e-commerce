6097fb602893330f125c58ae2f79eda658bace83
Sebastian <sebastian.kosciolek2000@gmail.com>
2024-08-26 11:10:07 +0200

    start created ecommerce app

--- src/payload/payload.config.ts
+++ src/payload/payload.config.ts
@@ -42,6 +42,7 @@ dotenv.config({
 export default buildConfig({
   admin: {
     user: Users.slug,
+    css: path.resolve(__dirname, '../app/_css/customTheme.scss'),
     bundler: webpackBundler(), // bundler-config
     components: {
       // The `BeforeLogin` component renders a message that you see while logging into your admin panel.
