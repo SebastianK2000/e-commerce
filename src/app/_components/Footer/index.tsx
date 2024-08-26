748c865f554adbd4bbccf93f6f03194337fa0d8e
Sebastian <sebastian.kosciolek2000@gmail.com>
2024-08-26 15:26:17 +0200

    created full footer

--- src/app/_components/Footer/index.tsx
+++ src/app/_components/Footer/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Footer } from '../../../payload/payload-types'
+import type { Footer } from '../../../payload/payload-types'
 import { fetchFooter } from '../../_api/fetchGlobals'
 import FooterComponent from './FooterComponent/index'
 
