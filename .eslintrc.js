5860bba7989a24b4992306ead6170d64cf5839bf
Sebastian <sebastian.kosciolek2000@gmail.com>
2024-08-28 15:46:29 +0200

    Complete app

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,15 @@ module.exports = {
   extends: [
     'plugin:@next/next/recommended',
     '@payloadcms',
-    'plugin:prettier/recommended', // Dodaj to, jeśli używasz Prettiera
-    'plugin:simple-import-sort/recommended' // Dodaj to, aby korzystać z sortowania importów
+    'plugin:prettier/recommended',
+    'plugin:simple-import-sort/recommended'
   ],
   ignorePatterns: ['**/payload-types.ts'],
-  plugins: ['prettier', 'simple-import-sort'], // Dodaj plugin simple-import-sort
+  plugins: ['prettier', 'simple-import-sort'],
   rules: {
     'prettier/prettier': ['error', { endOfLine: 'auto' }],
     'no-console': 'off',
-    'simple-import-sort/imports': 'error', // Dodaj regułę sortowania importów
-    'simple-import-sort/exports': 'error'  // Dodaj regułę sortowania eksportów
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error'
   },
 }
